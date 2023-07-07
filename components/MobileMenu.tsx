9b43a390bded5cb606c3a27d1af136770efa11a1
David Williams <85596817+SafetyDav3@users.noreply.github.com>
2023-07-06 19:34:13 -0500

    MobileMenu extracted

---
+++ components/MobileMenu.tsx
@@ -0,0 +1,187 @@
+"use client";
+
+import { Fragment, useState } from "react";
+import { Dialog, Tab, Transition } from "@headlessui/react";
+
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+
+import { currencies, navigation } from "@/constants";
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
+
+const MobileMenu = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <Transition.Root show={mobileMenuOpen} as={Fragment}>
+      <Dialog
+        as="div"
+        className="relative z-40 lg:hidden"
+        onClose={setMobileMenuOpen}
+      >
+        <Transition.Child
+          as={Fragment}
+          enter="transition-opacity ease-linear duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="transition-opacity ease-linear duration-300"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-40 flex">
+          <Transition.Child
+            as={Fragment}
+            enter="transition ease-in-out duration-300 transform"
+            enterFrom="-translate-x-full"
+            enterTo="translate-x-0"
+            leave="transition ease-in-out duration-300 transform"
+            leaveFrom="translate-x-0"
+            leaveTo="-translate-x-full"
+          >
+            <Dialog.Panel className="relative flex w-full max-w-xs flex-col overflow-y-auto bg-white pb-12 shadow-xl">
+              <div className="flex px-4 pb-2 pt-5">
+                <button
+                  type="button"
+                  className="-m-2 inline-flex items-center justify-center rounded-md p-2 text-gray-400"
+                  onClick={() => setMobileMenuOpen(false)}
+                >
+                  <span className="sr-only">Close menu</span>
+                  <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+              </div>
+
+              {/* Links */}
+              <Tab.Group as="div" className="mt-2">
+                <div className="border-b border-gray-200">
+                  <Tab.List className="-mb-px flex space-x-8 px-4">
+                    {navigation.categories.map((category) => (
+                      <Tab
+                        key={category.name}
+                        className={({ selected }) =>
+                          classNames(
+                            selected
+                              ? "border-indigo-600 text-indigo-600"
+                              : "border-transparent text-gray-900",
+                            "flex-1 whitespace-nowrap border-b-2 px-1 py-4 text-base font-medium"
+                          )
+                        }
+                      >
+                        {category.name}
+                      </Tab>
+                    ))}
+                  </Tab.List>
+                </div>
+                <Tab.Panels as={Fragment}>
+                  {navigation.categories.map((category) => (
+                    <Tab.Panel
+                      key={category.name}
+                      className="space-y-12 px-4 py-6"
+                    >
+                      <div className="grid grid-cols-2 gap-x-4 gap-y-10">
+                        {category.featured.map((item) => (
+                          <div key={item.name} className="group relative">
+                            <div className="aspect-h-1 aspect-w-1 overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
+                              <img
+                                src={item.imageSrc}
+                                alt={item.imageAlt}
+                                className="object-cover object-center"
+                              />
+                            </div>
+                            <a
+                              href={item.href}
+                              className="mt-6 block text-sm font-medium text-gray-900"
+                            >
+                              <span
+                                className="absolute inset-0 z-10"
+                                aria-hidden="true"
+                              />
+                              {item.name}
+                            </a>
+                            <p
+                              aria-hidden="true"
+                              className="mt-1 text-sm text-gray-500"
+                            >
+                              Shop now
+                            </p>
+                          </div>
+                        ))}
+                      </div>
+                    </Tab.Panel>
+                  ))}
+                </Tab.Panels>
+              </Tab.Group>
+
+              <div className="space-y-6 border-t border-gray-200 px-4 py-6">
+                {navigation.pages.map((page) => (
+                  <div key={page.name} className="flow-root">
+                    <a
+                      href={page.href}
+                      className="-m-2 block p-2 font-medium text-gray-900"
+                    >
+                      {page.name}
+                    </a>
+                  </div>
+                ))}
+              </div>
+
+              <div className="space-y-6 border-t border-gray-200 px-4 py-6">
+                <div className="flow-root">
+                  <a
+                    href="#"
+                    className="-m-2 block p-2 font-medium text-gray-900"
+                  >
+                    Create an account
+                  </a>
+                </div>
+                <div className="flow-root">
+                  <a
+                    href="#"
+                    className="-m-2 block p-2 font-medium text-gray-900"
+                  >
+                    Sign in
+                  </a>
+                </div>
+              </div>
+
+              <div className="space-y-6 border-t border-gray-200 px-4 py-6">
+                {/* Currency selector */}
+                <form>
+                  <div className="inline-block">
+                    <label htmlFor="mobile-currency" className="sr-only">
+                      Currency
+                    </label>
+                    <div className="group relative -ml-2 rounded-md border-transparent focus-within:ring-2 focus-within:ring-white">
+                      <select
+                        id="mobile-currency"
+                        name="currency"
+                        className="flex items-center rounded-md border-transparent bg-none py-0.5 pl-2 pr-5 text-sm font-medium text-gray-700 focus:border-transparent focus:outline-none focus:ring-0 group-hover:text-gray-800"
+                      >
+                        {currencies.map((currency) => (
+                          <option key={currency}>{currency}</option>
+                        ))}
+                      </select>
+                      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center">
+                        <ChevronDownIcon
+                          className="h-5 w-5 text-gray-500"
+                          aria-hidden="true"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </Dialog.Panel>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+};
+
+export default MobileMenu;
---
+++ components/index.ts
@@ -0,0 +1,4 @@
+import Footer from "./Footer";
+import MobileMenu from "./MobileMenu";
+
+export { Footer, MobileMenu };
