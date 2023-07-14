59033ec96e9c87c8c4da616376b50bf02795a894
David Williams <85596817+SafetyDav3@users.noreply.github.com>
2023-07-13 20:03:10 -0500

    scratch dat

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
-import { Footer, Hero } from "@/components";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,9 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Hero />
         {children}
-        <Footer />
       </body>
     </html>
   );
--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,2 @@
-import Footer from "./Footer";
-import MobileMenu from "./MobileMenu";
-import Hero from "./Hero";
-import Header from "./Header";
-import Cart from "./Cart";
 
-export { Footer, MobileMenu, Hero, Header, Cart };
+
